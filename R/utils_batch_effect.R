365defcd3f6b16363115f4f17a9e1f050b43233f
Yaoxiang Li <liyaoxiang@outlook.com>
2024-07-02 01:43:40 -0400

    updated

--- R/utils_batch_effect.R
+++ R/utils_batch_effect.R
@@ -1,6 +1,6 @@
-#' Perform Principal Variance Component Analysis (PVCA) for Batch Effect Assessment
+#' Perform Principal Variance Component Analysis for Batch Effect Assessment
 #'
-#' This function performs Principal Variance Component Analysis (PVCA) to assess batch effects in the dataset.
+#' This function performs Principal Variance Component Analysis to assess batch effects in the dataset.
 #'
 #' @param data_matrix A data frame or matrix where rows represent features and columns represent samples.
 #' @param sample_info A data frame containing sample information with rows matching the columns of `data_matrix`.
@@ -8,10 +8,35 @@
 #' @param threshold A numeric value between 0 and 1 to specify the PVCA threshold.
 #'
 #' @return A PVCA object containing the results of the batch effect assessment.
-#' @import Biobase pvca
+#' @importFrom Biobase ExpressionSet
+#' @importFrom pvca pvcaBatchAssess
 #' @export
 #'
 #' @examples
+#' # Example data
+#' set.seed(123)
+#' data_matrix <- data.frame(
+#'   sample1 = rnorm(100),
+#'   sample2 = rnorm(100),
+#'   sample3 = rnorm(100),
+#'   sample4 = rnorm(100),
+#'   sample5 = rnorm(100),
+#'   sample6 = rnorm(100),
+#'   sample7 = rnorm(100),
+#'   sample8 = rnorm(100),
+#'   sample9 = rnorm(100),
+#'   sample10 = rnorm(100)
+#' )
+#' rownames(data_matrix) <- paste0("feature", 1:100)
+#'
+#' sample_info <- data.frame(
+#'   dose = c(0, 1, 2, 1, 0, 2, 1, 0, 2, 1),
+#'   time = c(-1, 1, 25, -1, 1, 25, -1, 1, 25, -1),
+#'   batch = rep(c("A", "B", "C"), length.out = 10)
+#' )
+#' rownames(sample_info) <- colnames(data_matrix)
+#'
+#' # Perform PVCA Batch Effect Assessment
 #' pvca_results <- perform_pvca_batch_assessment(data_matrix, sample_info, c("batch", "dose", "time"), 0.6)
 perform_pvca_batch_assessment <- function(data_matrix, sample_info, batch_effects, threshold) {
   cli::cli_h1("Starting PVCA Batch Effect Assessment")
@@ -37,12 +62,27 @@ perform_pvca_batch_assessment <- function(data_matrix, sample_info, batch_effect
     stop("Input `threshold` must be a numeric value between 0 and 1.")
   }
 
-  cli::cli_alert_info("Transposing and converting data to matrix format.")
-  data_matrix <- data.matrix(t(data_matrix))
+  if (all(colnames(data_matrix) == rownames(sample_info))) {
+    cli::cli_alert_info("Sample names match between columns of `data_matrix` and rows of `sample_info`. No transposition needed.")
+    data_matrix <- data.matrix(data_matrix)
+  } else if (all(rownames(data_matrix) == rownames(sample_info))) {
+    cli::cli_alert_info("Sample names match between rows of `data_matrix` and rows of `sample_info`. Transposing `data_matrix`.")
+    data_matrix <- data.matrix(t(data_matrix))
+  } else {
+    cli::cli_alert_danger("Sample names do not match between `data_matrix` and `sample_info`.")
+    stop("Sample names do not match between `data_matrix` and `sample_info`.")
+  }
+
   data_matrix[is.na(data_matrix)] <- 0
 
-  cli::cli_alert_info("Matching sample information to the data matrix.")
-  sample_info <- sample_info[colnames(data_matrix), ]
+  # Align sample names
+  common_samples <- intersect(colnames(data_matrix), rownames(sample_info))
+  if (length(common_samples) != ncol(data_matrix) || length(common_samples) != nrow(sample_info)) {
+    cli::cli_alert_danger("Sample names in `data_matrix` do not match `sample_info`.")
+    stop("Sample names in `data_matrix` do not match `sample_info`.")
+  }
+  data_matrix <- data_matrix[, common_samples]
+  sample_info <- sample_info[common_samples, ]
 
   cli::cli_alert_info("Creating AnnotatedDataFrame.")
   pheno_data <- new("AnnotatedDataFrame", data = sample_info)
@@ -61,22 +101,23 @@ perform_pvca_batch_assessment <- function(data_matrix, sample_info, batch_effect
 }
 
 
-#' @import Biobase pvca
-#' @export
-pvcaBF <- function(df, sampleInfo, batch_effect, threshold) {
-  print("pvcaBF start")
-  df <- data.matrix(t(df))
-  #df[is.na(df)]<-0
-  #df<-df[,rownames(sampleInfo)]
-  sampleInfo <- sampleInfo[colnames(df),]
-  phenoData <- new("AnnotatedDataFrame", data = sampleInfo)
-  myExpressionSet <- Biobase::ExpressionSet(assayData = df,
-                                            phenoData = phenoData)
-  pvcaObj <-
-    pcva::pvcaBatchAssess(myExpressionSet, batch_effect, threshold)
-  print("pvcaBF end")
-  return(pvcaObj)
-}
+
+
+
+# pvcaBF <- function(df, sampleInfo, batch_effect, threshold) {
+#   print("pvcaBF start")
+#   df <- data.matrix(t(df))
+#   #df[is.na(df)]<-0
+#   #df<-df[,rownames(sampleInfo)]
+#   sampleInfo <- sampleInfo[colnames(df),]
+#   phenoData <- new("AnnotatedDataFrame", data = sampleInfo)
+#   myExpressionSet <- Biobase::ExpressionSet(assayData = df,
+#                                             phenoData = phenoData)
+#   pvcaObj <-
+#     pcva::pvcaBatchAssess(myExpressionSet, batch_effect, threshold)
+#   print("pvcaBF end")
+#   return(pvcaObj)
+# }
 
 #' import ggplot2
 #' @export
