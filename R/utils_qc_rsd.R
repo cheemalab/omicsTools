a867615ea848fb03a1fa842d86967f0e4eed5552
Yaoxiang Li <liyaoxiang@outlook.com>
2024-07-09 12:13:06 -0400

    updated

--- R/utils_qc_rsd.R
+++ R/utils_qc_rsd.R
@@ -1,26 +1,65 @@
-#' Calculate the relative standard deviation for QC samples
+#' Calculate QC Statistics and RSD
 #'
-#' @param normalized_tibble A tibble normalized by 'is_normalize' or similar function.
-#' @param qc_name Character string specifying the name of the QC sample. Defaults to "Pooled QC".
-#' @return A tibble containing the mean, standard deviation, and coefficient of variation for QC samples.
+#' This function calculates the mean, standard deviation, and coefficient of variation
+#' for QC data, optionally ignoring NA values. It also calculates the relative standard deviation (RSD)
+#' for both Pooled QC and NIST samples. The input data should be a tibble where
+#' the first column is the sample ID and all other columns are features.
 #'
+#' @param data A tibble with the first column as sample IDs and other columns as features.
+#' @param qc_strings A string pattern to filter Pooled QC samples. Default is "Pooled QC|Pooled_QC".
+#' @param nist_strings A string pattern to filter NIST samples. Default is "NIST".
+#' @param id_col The name of the sample ID column. Default is "sample_id".
+#' @param ignore_na Logical, whether to ignore NA values in calculations. Default is TRUE.
+#' @return A list containing tibbles with calculated statistics for Pooled QC and NIST samples. If no matching samples are found, returns NULL for that category.
+#' @importFrom dplyr filter mutate
+#' @importFrom tidyr pivot_longer pivot_wider
+#' @importFrom rlang sym
+#' @importFrom matrixStats rowSds
+#' @author Yaoxiang Li
 #' @export
-#' @author Yaoxiang Li \email{yl814@georgetown.edu}
+#' @examples
+#' # Generate example data
+#' example_data <- tibble::tibble(
+#'   sample_id = c("Pooled QC 1", "Pooled QC 2", "NIST 1", "NIST 2", "Sample 1"),
+#'   feature1 = c(10, 15, 20, NA, 5),
+#'   feature2 = c(20, NA, 25, 30, 10),
+#'   feature3 = c(NA, 25, 30, 35, 15)
+#' )
 #'
-#' Georgetown University, USA
+#' # Calculate QC statistics and RSD
+#' rsd_stats <- calculate_qc_rsd(example_data)
 #'
-#' License: GPL (>= 3)
-qc_rsd <- function(normalized_tibble, qc_name = "Pooled QC") {
-  qc_tibble <- normalized_tibble %>% dplyr::filter(grepl(qc_name, Sample))
-  df.transposed <- qc_tibble %>%
-    tidyr::pivot_longer(cols= -1) %>%
-    tidyr::pivot_wider(names_from = "Sample",values_from = "value")
-  qc_nm <- colnames(df.transposed)[-1]
-  df.transposed <- df.transposed %>%
-    mutate(Mean = rowMeans(.[qc_nm]),
-           `Standard Deviation` = matrixStats::rowSds(as.matrix(.[qc_nm])),
-           `Coefficient of variation` = `Standard Deviation` / Mean)
-  return(df.transposed)
+#' # Access results
+#' pooled_qc_rsd <- rsd_stats$Pooled_QC_RSD
+#' nist_rsd <- rsd_stats$NIST_RSD
+calculate_qc_rsd <- function(data, qc_strings = "Pooled QC|Pooled_QC", nist_strings = "NIST", id_col = "sample_id", ignore_na = TRUE) {
+  calculate_qc_statistics <- function(data, filter_strings, id_col, ignore_na) {
+    qc_tibble <- data |> dplyr::filter(grepl(filter_strings, !!rlang::sym(id_col)))
+
+    if (nrow(qc_tibble) == 0) {
+      return(NULL)
+    }
+
+    df.transposed <- qc_tibble |>
+      tidyr::pivot_longer(cols = -all_of(id_col)) |>
+      tidyr::pivot_wider(names_from = all_of(id_col), values_from = "value")
+
+    qc_nm <- colnames(df.transposed)[-1]
+
+    df.transposed <- df.transposed %>%
+      dplyr::mutate(
+        Mean = rowMeans(.[qc_nm], na.rm = ignore_na),
+        `Standard Deviation` = matrixStats::rowSds(as.matrix(.[qc_nm]), na.rm = ignore_na),
+        `Coefficient of variation` = `Standard Deviation` / Mean
+      )
+
+    return(df.transposed)
+  }
+
+  pooled_qc_stats <- calculate_qc_statistics(data, qc_strings, id_col, ignore_na)
+  nist_stats <- calculate_qc_statistics(data, nist_strings, id_col, ignore_na)
+
+  return(list(Pooled_QC_RSD = pooled_qc_stats, NIST_RSD = nist_stats))
 }
 
-utils::globalVariables(c(".", "Standard Deviation", "Mean"))
+
